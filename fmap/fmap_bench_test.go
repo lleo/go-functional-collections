14c912798057fa092eee737a6d9a58bceea6ceee
LLeo <lleoem@gmail.com>
2017-12-15 08:29:59 -0500

    removed old commented out code

--- fmap/fmap_bench_test.go
+++ fmap/fmap_bench_test.go
@@ -76,14 +76,6 @@ var XtraKvs100M []KeyVal
 var XtraKvs1MM []KeyVal
 var XtraKvs10MM []KeyVal
 
-func benchPutOne(xtra []KeyVal, m *fmap.Map, b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		var j = rand.Int() % len(xtra)
-		var kv = xtra[j]
-		_ = m.Put(kv.Key, kv.Val)
-	}
-}
-
 func BenchmarkPutOne10(b *testing.B) {
 	var xtra = XtraKvs10
 	var m = FMap10
@@ -95,7 +87,12 @@ func BenchmarkPutOne10(b *testing.B) {
 		FMap10 = m
 	}
 	b.ResetTimer()
-	benchPutOne(xtra, m, b)
+
+	for i := 0; i < b.N; i++ {
+		var j = rand.Int() % len(xtra)
+		var kv = xtra[j]
+		_ = m.Put(kv.Key, kv.Val)
+	}
 }
 
 func BenchmarkPutOne100(b *testing.B) {
@@ -110,7 +107,12 @@ func BenchmarkPutOne100(b *testing.B) {
 		FMap100 = m
 	}
 	b.ResetTimer()
-	benchPutOne(xtra, m, b)
+
+	for i := 0; i < b.N; i++ {
+		var j = rand.Int() % len(xtra)
+		var kv = xtra[j]
+		_ = m.Put(kv.Key, kv.Val)
+	}
 }
 
 func BenchmarkPutOne1M(b *testing.B) {
@@ -125,7 +127,12 @@ func BenchmarkPutOne1M(b *testing.B) {
 		FMap1M = m
 	}
 	b.ResetTimer()
-	benchPutOne(xtra, m, b)
+
+	for i := 0; i < b.N; i++ {
+		var j = rand.Int() % len(xtra)
+		var kv = xtra[j]
+		_ = m.Put(kv.Key, kv.Val)
+	}
 }
 
 func BenchmarkPutOne10M(b *testing.B) {
@@ -140,7 +147,12 @@ func BenchmarkPutOne10M(b *testing.B) {
 		FMap10M = m
 	}
 	b.ResetTimer()
-	benchPutOne(xtra, m, b)
+
+	for i := 0; i < b.N; i++ {
+		var j = rand.Int() % len(xtra)
+		var kv = xtra[j]
+		_ = m.Put(kv.Key, kv.Val)
+	}
 }
 
 func BenchmarkPutOne100M(b *testing.B) {
@@ -155,7 +167,12 @@ func BenchmarkPutOne100M(b *testing.B) {
 		FMap100M = m
 	}
 	b.ResetTimer()
-	benchPutOne(xtra, m, b)
+
+	for i := 0; i < b.N; i++ {
+		var j = rand.Int() % len(xtra)
+		var kv = xtra[j]
+		_ = m.Put(kv.Key, kv.Val)
+	}
 }
 
 func BenchmarkPutOne1MM(b *testing.B) {
@@ -170,7 +187,12 @@ func BenchmarkPutOne1MM(b *testing.B) {
 		FMap1MM = m
 	}
 	b.ResetTimer()
-	benchPutOne(xtra, m, b)
+
+	for i := 0; i < b.N; i++ {
+		var j = rand.Int() % len(xtra)
+		var kv = xtra[j]
+		_ = m.Put(kv.Key, kv.Val)
+	}
 }
 
 func BenchmarkPutOne10MM(b *testing.B) {
@@ -185,411 +207,10 @@ func BenchmarkPutOne10MM(b *testing.B) {
 		FMap10MM = m
 	}
 	b.ResetTimer()
-	benchPutOne(xtra, m, b)
-}
 
-//func buildKeys(numKeys int) []StringKey {
-//	var keys = make([]StringKey, numKeys)
-//	var s = "a"
-//	for i := 0; i < numKeys; i++ {
-//		keys[i] = StringKey(s)
-//		s = Inc(s)
-//	}
-//	return keys
-//}
-//
-//var NumKey10MM = BaseMap10MMSize + (50 * 1000)
-//var Keys10MM []StringKey
-//
-//var BaseMap10xSize = 10
-//var BaseMap10x *fmap.Map
-//
-//func Benchmark_BaseMap10x_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap10x_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n", NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap10x
-//	if m == nil {
-//		log.Println("Making Map...")
-//
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap10xSize; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap10x = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap10xSize
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
-//var BaseMap10Size = 10
-//var BaseMap10 *fmap.Map
-//
-//func Benchmark_BaseMap10_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap10_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n", NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap10
-//	if m == nil {
-//		log.Println("Making Map...")
-//
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap10Size; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap10 = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap10Size
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
-//var BaseMap100Size = 100
-//var BaseMap100 *fmap.Map
-//
-//func Benchmark_BaseMap100_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap100_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n", NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap100
-//	if m == nil {
-//		log.Println("Making Map...")
-//
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap100Size; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap100 = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap100Size
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
-//var BaseMap1MSize = 1 * 1000
-//var BaseMap1M *fmap.Map
-//
-//func Benchmark_BaseMap1M_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap1M_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n", NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap1M
-//	if m == nil {
-//		log.Println("Making Map...")
-//
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap1MSize; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap1M = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap1MSize
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
-//var BaseMap10MSize = 10 * 1000
-//var BaseMap10M *fmap.Map
-//
-//func Benchmark_BaseMap10M_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap10M_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n", NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap10M
-//	if m == nil {
-//		log.Println("Making Map...")
-//
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap10MSize; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap10M = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap10MSize
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
-//var BaseMap100MSize = 100 * 1000
-//var BaseMap100M *fmap.Map
-//
-//func Benchmark_BaseMap100M_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap100M_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n", NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap100M
-//	if m == nil {
-//		log.Println("Making Map...")
-//
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap100MSize; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap100M = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap100MSize
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
-//var BaseMap1MMSize = 1 * 1000 * 1000
-//var BaseMap1MM *fmap.Map
-//
-//func Benchmark_BaseMap1MM_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap1MM_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n",
-//			NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap1MM
-//	if m == nil {
-//		log.Println("Making Map...")
-//
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap1MMSize; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap1MM = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap1MMSize
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
-//var BaseMap10MMSize = 10 * 1000 * 1000
-//var BaseMap10MM *fmap.Map
-//
-//func Benchmark_BaseMap10MM_PutOne(b *testing.B) {
-//	log.Printf("Benchmark_BaseMap10MM_PutOne: b.N=%d\n", b.N)
-//
-//	var keys = Keys10MM
-//	if keys == nil {
-//		log.Printf("Making Keys10MM=%d...\n", NumKey10MM)
-//
-//		var startKeys = time.Now()
-//		keys = buildKeys(NumKey10MM)
-//		var tookKeys = time.Since(startKeys)
-//
-//		log.Printf("build keys took       -> %s\n", tookKeys)
-//
-//		Keys10MM = keys
-//
-//		log.Printf("Made Keys10MM; last key = Keys10MM[%d]=%s\n",
-//			len(keys), keys[len(keys)-1])
-//	}
-//
-//	var m = BaseMap10MM
-//	if m == nil {
-//		log.Println("Making Map...")
-//		var startMap = time.Now()
-//		m = fmap.New()
-//		for i := 0; i < BaseMap10MMSize; i++ {
-//			m = m.Put(keys[i], i)
-//		}
-//		var tookMap = time.Since(startMap)
-//
-//		log.Printf("build Map took        -> %s\n", tookMap)
-//
-//		BaseMap10MM = m
-//	}
-//
-//	b.ResetTimer()
-//
-//	var keyStart = BaseMap10MMSize
-//	keys = keys[keyStart : keyStart+2000]
-//
-//	for i := 0; i < b.N; i++ {
-//		var j = i % 2000
-//		_ = m.Put(keys[j], i)
-//	}
-//}
-//
+	for i := 0; i < b.N; i++ {
+		var j = rand.Int() % len(xtra)
+		var kv = xtra[j]
+		_ = m.Put(kv.Key, kv.Val)
+	}
+}
