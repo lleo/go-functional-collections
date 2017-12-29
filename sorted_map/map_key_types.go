8374f7509c27dbadc02a04c084722a2de750615f
LLeo <lleoem@gmail.com>
2017-12-29 12:14:38 -0500

    moved IntKey to smap_test.go & deleted StringKey

--- sorted_map/map_key_types.go
+++ sorted_map/map_key_types.go
@@ -1,31 +1,17 @@
 package sorted_map
 
-import "strconv"
+//import "strconv"
 
-type StringKey string
-
-func (sk StringKey) Less(o MapKey) bool {
-	var osk, ok = o.(StringKey)
-	if !ok {
-		panic("o is not a StringKey")
-	}
-	return sk < osk
-}
-
-func (sk StringKey) String() string {
-	return string(sk)
-}
-
-type IntKey int
-
-func (ik IntKey) Less(o MapKey) bool {
-	var oik, ok = o.(IntKey)
-	if !ok {
-		panic("o is not a IntKey")
-	}
-	return ik < oik
-}
-
-func (ik IntKey) String() string {
-	return strconv.Itoa(int(ik))
-}
+//type IntKey int
+//
+//func (ik IntKey) Less(o MapKey) bool {
+//	var oik, ok = o.(IntKey)
+//	if !ok {
+//		panic("o is not a IntKey")
+//	}
+//	return ik < oik
+//}
+//
+//func (ik IntKey) String() string {
+//	return strconv.Itoa(int(ik))
+//}
